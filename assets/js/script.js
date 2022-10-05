dec4d4db4626be49f8a9ed7c123be8ffad5b68ab
diabm1 <diabm1855@gmail.com>
2022-10-05 16:35:03 -0400

    trying to figure out how to display score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,26 @@
 // buttonClicked = document.getElementById("button");
 // buttonClicked.addEventListener("click", getComputerChoice);
 
-rockClicked = document.getElementById("rock");
-rockClicked.addEventListener("click", displayRock);
-rockClicked.addEventListener("click", getComputerChoice)
-
-paperClicked = document.getElementById("paper");
-paperClicked.addEventListener("click", displayPaper);
-paperClicked.addEventListener("click", getComputerChoice)
-
-scissorsClicked = document.getElementById("scissors");
-scissorsClicked.addEventListener("click", displayScissors);
-scissorsClicked.addEventListener("click", getComputerChoice)
-
 let displayBox = document.getElementById("displayBoxForUser");
 let displayBoxForComp = document.getElementById("displayBoxForComputer");
+let displayOutcome = document.getElementById("displayOutcome");
+
+let rockClicked = document.getElementById("rock");
+rockClicked.addEventListener("click", displayRock);
+rockClicked.addEventListener("click", getComputerChoice);
+
+
+let paperClicked = document.getElementById("paper");
+paperClicked.addEventListener("click", displayPaper);
+paperClicked.addEventListener("click", getComputerChoice);
+paperClicked.addEventListener("click", decidesDraw);
+
+let scissorsClicked = document.getElementById("scissors");
+scissorsClicked.addEventListener("click", displayScissors);
+scissorsClicked.addEventListener("click", getComputerChoice);
+scissorsClicked.addEventListener("click", decidesDraw);
+
+
 
 //User can select a rock, paper or scissors
 //if user clicks display either rock, paper, scissor
@@ -43,14 +49,15 @@ function displayScissors() {
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomNum = Math.floor(Math.random() * 3);
+  const compChoice = choices[randomNum];
   // return choices[randomNum]
-  console.log(choices[randomNum]);
-  if (choices[randomNum] == "rock") {
+  console.log(compChoice);
+  if (compChoice == "rock") {
     displayBoxForComp.innerHTML = `<h3>Computer has chosen rock</h3>`;
     // displayBoxForComp.innerHTML = `<h2>Draw!</h2>`;
-  } else if(choices[randomNum] == "paper"){
+  } else if (compChoice == "paper") {
     displayBoxForComp.innerHTML = `<h3>Computer has chosen paper</h3>`;
-  } else if(choices[randomNum] == "scissors"){
+  } else if (compChoice == "scissors") {
     displayBoxForComp.innerHTML = `<h3>Computer has chosen scissors</h3>`;
   }
 }
@@ -66,14 +73,27 @@ function decidesWinner() {
   //how do I get computer choice? How Do I extract it from the getComputerChoice? is it my lack of knowledge of js that is making this difficult or just straight up my logic? if logic then shouldn't I use a program like flowgorithm to plan out my logic?
 }
 
-
 function decidesLoser() {}
 
-function decidesDraw() {}
+function decidesDraw() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNum = Math.floor(Math.random() * 3);
+  const compChoice = choices[randomNum];
 
-
+  if (compChoice == "rock" && displayRock) {
+    displayOutcome.innerHTML = `<h2>Draw!</h2>`;
+    // rockClicked.addEventListener("click", decidesDraw);
+  } else if (compChoice == "paper" && displayPaper) {
+    displayOutcome.innerHTML = `<h2>Draw!</h2>`;
+  } else if (compChoice == "scissors" && displayScissors) {
+    displayOutcome.innerHTML = `<h2>Draw!</h2>`;
+  }
+}
 
 //A score of total wins and losses will displayed at the bottom of the screen
-function keepScore() {}
+function keepScore() {
+  //how do I calculate user score
+  //how do I calculate computer score
+}
 
 //The user can play as many times as they want
